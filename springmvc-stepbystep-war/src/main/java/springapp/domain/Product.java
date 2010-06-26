2f165a1df8a49e0326d6782ed68c670e4d03edf9
Jack Leow <jackgene@gmail.com>
2010-06-26 13:01:23 -0500

    Base Spring MVC Step-by-step tutorial.

---
+++ springmvc-stepbystep-war/src/main/java/springapp/domain/Product.java
@@ -0,0 +1,38 @@
+package springapp.domain;
+
+public class Product {
+    private int id;
+    private String description;
+    private Double price;
+    
+    public void setId(int i) {
+        id = i;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public Double getPrice() {
+        return price;
+    }
+    
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("Description: " + description + ";");
+        buffer.append("Price: " + price);
+        return buffer.toString();
+    }
+}
